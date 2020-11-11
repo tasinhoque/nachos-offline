35745527f5341a4511b6fb26b93fe2f578f129f9
Enan Ajmain <3nan.ajmain@gmail.com>
2020-11-12 02:36:43 +0600

    Finish task 3 with test
    The method threads/Alarm.java#waitUntil is implemented. The associated
    test is written in threads/Alarm.java#selfTest().

--- src/nachos/threads/Alarm.java
+++ src/nachos/threads/Alarm.java
@@ -2,6 +2,8 @@ package nachos.threads;
 
 import nachos.machine.*;
 
+import java.util.PriorityQueue;
+
 /**
  * Uses the hardware timer to provide preemption, and to allow threads to sleep
  * until a certain time.
@@ -29,7 +31,15 @@ public class Alarm {
      * that should be run.
      */
     public void timerInterrupt() {
-        KThread.currentThread().yield();
+        boolean inStatus=Machine.interrupt().disable();
+        while(!waitQueue.isEmpty()
+                && waitQueue.peek().wakeTime <= Machine.timer().getTime()) {
+            System.out.println("Waking up thread " +  waitQueue.peek().thread.toString()
+                    + " at " + Machine.timer().getTime() + " cycles");
+            waitQueue.poll().thread.ready();
+        }
+        KThread.yield();
+        Machine.interrupt().restore(inStatus);
     }
 
     /**
@@ -46,10 +56,56 @@ public class Alarm {
      *
      * @see	nachos.machine.Timer#getTime()
      */
+    // Todo: Part 1, Task 3
     public void waitUntil(long x) {
         // for now, cheat just to get something working (busy waiting is bad)
         long wakeTime = Machine.timer().getTime() + x;
-        while (wakeTime > Machine.timer().getTime())
-            KThread.yield();
+
+        boolean inStatus = Machine.interrupt().disable();
+        System.out.println("At " + (wakeTime - x) + " cycles, sleeping thread "
+                + KThread.currentThread().toString() + " until " + wakeTime + " cycles");
+        waitQueue.add(new WaitThread(KThread.currentThread(), wakeTime));
+        KThread.sleep();
+        Machine.interrupt().restore(inStatus);
+    }
+
+    private class WaitThread implements Comparable<WaitThread> {
+        KThread thread;
+        long wakeTime;
+
+        public WaitThread(KThread thread, long wakeTime){
+            this.thread = thread;
+            this.wakeTime = wakeTime;
+        }
+
+        public int compareTo(WaitThread thread){
+            return Long.compare(this.wakeTime, thread.wakeTime);
+
+        }
+    }
+
+    private PriorityQueue<WaitThread> waitQueue = new PriorityQueue<>();
+    private static final char dbgAlarm = 'w';
+
+    public static void selfTest() {
+        class RunAlarm implements Runnable {
+            @Override
+            public void run() {
+                long tick = (int)(Math.random() * 1000) + 500;
+                ThreadedKernel.alarm.waitUntil(tick);
+            }
+        }
+
+        Lib.debug(dbgAlarm, "Enter Alarm.selfTest");
+
+        RunAlarm runAlarm = new RunAlarm();
+
+        for (int i = 0; i < 10; i++) {
+            KThread thread = new KThread(runAlarm).setName("Alarm-" + i);
+            thread.fork();
+        }
+
+        Lib.debug(dbgAlarm, "Exit Alarm.selfTest");
+
     }
 }
