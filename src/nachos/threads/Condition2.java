a61ebeab78bcbb3c08e45a610ff533bf718b6745
Tasin Hoque <tasinhoque@gmail.com>
2020-11-12 11:15:17 +0600

    Finish task 2

--- src/nachos/threads/Condition2.java
+++ src/nachos/threads/Condition2.java
@@ -22,6 +22,7 @@ public class Condition2 {
      */
     public Condition2(Lock conditionLock) {
         this.conditionLock = conditionLock;
+        waitQueue = ThreadedKernel.scheduler.newThreadQueue(false);
     }
 
     /**
@@ -35,6 +36,11 @@ public class Condition2 {
 
         conditionLock.release();
 
+        boolean inStatus = Machine.interrupt().disable();
+        waitQueue.waitForAccess(KThread.currentThread());
+        KThread.sleep();
+        Machine.interrupt().restore(inStatus);
+
         conditionLock.acquire();
     }
 
@@ -44,6 +50,14 @@ public class Condition2 {
      */
     public void wake() {
         Lib.assertTrue(conditionLock.isHeldByCurrentThread());
+
+        boolean inStatus = Machine.interrupt().disable();
+        KThread thread = waitQueue.nextThread();
+
+        if (thread != null)
+            thread.ready();
+
+        Machine.interrupt().restore(inStatus);
     }
 
     /**
@@ -52,7 +66,80 @@ public class Condition2 {
      */
     public void wakeAll() {
         Lib.assertTrue(conditionLock.isHeldByCurrentThread());
+
+        boolean inStatus = Machine.interrupt().disable();
+        while (true) {
+            KThread thread = waitQueue.nextThread();
+            if (thread == null)
+                break;
+            thread.ready();
+        }
+        Machine.interrupt().restore(inStatus);
+    }
+
+    public static void selfTest() {
+        final Lock lock = new Lock();
+        final Condition2 condition = new Condition2(lock);
+        int count = 0;
+
+        class RunCondition2 implements Runnable {
+            int id = -1;
+
+            RunCondition2(int count) {
+                id = count++;
+            }
+
+            @Override
+            public void run() {
+                lock.acquire();
+                System.out.println("Thread " + id + " is going to sleep");
+                condition.sleep();
+                System.out.println("Thread " + id + " has been woken up");
+                lock.release();
+            }
+        }
+
+        System.out.println("Condition TEST #1: Start");
+
+        KThread thread1 = new KThread(new RunCondition2(++count));
+        KThread thread2 = new KThread(new RunCondition2(++count));
+        KThread thread3 = new KThread(new RunCondition2(++count));
+
+        thread1.fork();
+        thread2.fork();
+        thread3.fork();
+
+        System.out.println("Main: yielding to run the other thread");
+        KThread.yield();
+        System.out.println("Main: sending the wake signal (to a single thread) then yeilding");
+        lock.acquire();
+        condition.wake();
+        lock.release();
+        KThread.yield();
+
+        System.out.println("Condition TEST #1: End\n");
+
+        System.out.println("Condition TEST #2: Start");
+
+        thread1 = new KThread(new RunCondition2(++count));
+        thread2 = new KThread(new RunCondition2(++count));
+        thread3 = new KThread(new RunCondition2(++count));
+
+        thread1.fork();
+        thread2.fork();
+        thread3.fork();
+
+        System.out.println("Main: yielding to run the other thread");
+        KThread.yield();
+        System.out.println("Main: sending the wake signal (to all threads) then yeilding");
+        lock.acquire();
+        condition.wakeAll();
+        lock.release();
+        KThread.yield();
+
+        System.out.println("Condition TEST #2: End");
     }
 
     private Lock conditionLock;
+    private ThreadQueue waitQueue;
 }
